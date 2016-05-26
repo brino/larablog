d7359876a16a154f9bb339aea9f45ba2ab5dfc91
Brian Mix <brian@brokerbin.com>
2016-05-26 07:15:38 -0500

    added google analytics tracking for production

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -86,7 +86,7 @@
     @yield('last')
 </div>
 {{env('APP_ENV')}}
-@if(env('APP_ENV','local') ==  'production')
+@if(App::environment('production'))
 <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
