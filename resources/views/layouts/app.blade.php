4ddf35102e0f810d41e7991ea167535950c75563
Brian Mix <brian@brokerbin.com>
2016-06-16 14:46:24 -0500

    move google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,18 @@
     <title>@yield('title') :: Brian Mix</title>
 
     @include('partials.head')
+    @if(App::environment('production'))
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', '{{ env('APP_ANALYTICS','UA-xxxxxxxxx-x') }}', 'auto');
+            ga('send', 'pageview');
+
+        </script>
+    @endif
 </head>
 <body>
 
@@ -85,17 +97,5 @@
     <script src="{{ asset('js/app.js') }}"></script>
     @yield('last')
 </div>
-@if(App::environment('production'))
-<script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-    ga('create', '{{ env('APP_ANALYTICS','UA-xxxxxxxxx-x') }}', 'auto');
-    ga('send', 'pageview');
-
-</script>
-@endif
 </body>
 </html>
