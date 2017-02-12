eeb2c4121491ad6842f69cf257b3b6584febeef5
Brian Mix <brian@brokerbin.com>
2017-02-12 11:41:20 -0600

    example.ini and readme updates

--- resources/views/partials/signature.blade.php
+++ resources/views/partials/signature.blade.php
@@ -1,8 +1,4 @@
-{{--@if(!empty($thing->user->avatarUrl()))--}}
-        {{--<span class="image is-16x16 pull-left"><img src="{{ $thing->user->avatarUrl() }}"></span>--}}
-{{--@endif&nbsp;--}}
 <a href="{{ route('profile',$thing->user) }}">
-    {{--<span class="icon"><i class="fa fa-user-circle"></i></span>--}}
     @if(empty($thing->user->avatarUrl()))
         <span class="icon is-small"><i class="fa fa-user-circle"></i></span>
     @else
@@ -10,14 +6,6 @@
     @endif
     {{ $thing->user->name }}
 </a>
-
-{{--@if(get_class($thing) == 'App\Article')--}}
-    {{--@can('update-article',$article)<a href="{{ route('article.edit',[$article]) }}">@endcan<span class="icon is-small"><i class="fa fa-pencil"></i></span>@can('update-article',$article)</a>@endcan--}}
-{{--@endif--}}
-
-{{--@if(get_class($thing) == 'App\Media')--}}
-    {{--@can('update-media',$media)<a href="{{ route('media.edit',[$media]) }}">@endcan<span class="icon is-small"><i class="fa fa-camera"></i></span>@can('update-media',$media)</a>@endcan--}}
-{{--@endif--}}
 
 @if(isset($thing->published_at))
     <span
@@ -31,11 +19,3 @@
 
     </span>
 @endif
-
-{{--<a href="{{ route('articles',[$thing->category,'query'=>request()->input('query'),'tags'=>request()->input('tags')]) }}">--}}
-    {{--{{ $thing->category->name }}--}}
-{{--</a>--}}
-
-{{--@if($thing->views)--}}
-    {{--<span class="icon is-small"><i class="fa fa-eye"></i></span> {{ number_format($thing->views) }}--}}
-{{--@endif--}}
