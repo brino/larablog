0d4e64e6abe485451213cf03208f9121095cf896
Brian Mix <brian@brokerbin.com>
2017-02-22 09:14:30 -0600

    removed APP_COPY in favor of config('app.name')

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="content has-text-centered">
             <p>
-                <span class="icon"><i class="fa fa-copyright"></i></span> 2016 brainmix.info
+                <span class="icon"><i class="fa fa-copyright"></i></span> 2016 {{ config('app.name') }}
             </p>
         </div>
     </div>
