7a2d18f18772eb8322ea562b37813be7b09750ed
Brian Mix <brian@brokerbin.com>
2017-03-19 17:17:44 -0500

    mail env var

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => env('APP_EMAIL','hello@example.com'),
+        'name' => env('APP_NAME','Example'),
     ],
 
     /*
